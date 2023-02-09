f205548c4a5b754d85311ce06d38934686ec66ff
camilo serna <camilo22serna22@gmail.com>
2023-02-09 16:10:13 -0500

    configuracion del proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeMainComponent } from './components/home-main/home-main.component';
+import { HeaderComponent } from './components/header/header.component';
 
+
+const appRoutes: Routes = [
+  {path:'holi',component:HomeMainComponent  },
+
+
+
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeMainComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes),
+    FormsModule,
+    ReactiveFormsModule, 
   ],
   providers: [],
   bootstrap: [AppComponent]
