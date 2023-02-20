51574b4e7985fbf83702012ca5c44905ca362af8
camilo serna <camilo22serna22@gmail.com>
2023-02-20 17:36:47 -0500

    se siguen haciendo cambios

--- src/app/components/list-all-aprendiz/list-all-aprendiz.component.ts
+++ src/app/components/list-all-aprendiz/list-all-aprendiz.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AprendizService } from 'src/app/services/aprendiz/aprendiz.service';
 
 @Component({
   selector: 'app-list-all-aprendiz',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./list-all-aprendiz.component.css']
 })
 export class ListAllAprendizComponent {
+
+  Aprendiz:any=[];
+
+  constructor(private serviceaprendiz:AprendizService){}
+
+  loaderAprendiz():void{
+    this.serviceaprendiz.listAprendiz().subscribe(
+      data =>{
+        console.log(data);
+        
+        this.Aprendiz = data;
+        console.log(this.Aprendiz);
+      }
+    )
+  }
 
 }
--- src/app/services/aprendiz/aprendiz.service.ts
+++ src/app/services/aprendiz/aprendiz.service.ts
@@ -14,7 +14,15 @@ export class AprendizService {
 
   saveAprendiz(newAprendiz: Aprendiz): Observable<Aprendiz>{
 
-    return this.httpcient.post<Aprendiz>("http://localhost:4200/aprendiz/save", newAprendiz);
+    return this.httpcient.post<Aprendiz>("http://localhost:8080/aprendiz/save", newAprendiz);
     
   }
+
+  listAprendiz():  Observable<Aprendiz>{
+    return this.httpcient.get<Aprendiz>("http://localhost:8080/aprendiz/consultall")
+  }
 }
+
+
+
+
