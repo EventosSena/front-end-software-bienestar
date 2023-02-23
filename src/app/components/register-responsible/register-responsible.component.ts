453351ad13f9c772bc2b9cad9264c458a1da1748
andres camilo <camilo22serna22@gmail.com>
2023-02-22 19:16:33 -0500

    se agregaron nuevos cambios

--- src/app/components/register-responsible/register-responsible.component.ts
+++ src/app/components/register-responsible/register-responsible.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { Responsible } from 'src/app/models/Responsible';
+import { ResponsibleService } from 'src/app/services/responsible/responsible.service';
 
 @Component({
   selector: 'app-register-responsible',
@@ -6,5 +9,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./register-responsible.component.css']
 })
 export class RegisterResponsibleComponent {
+
+  responsible:Responsible= new Responsible();
+
+  constructor(private router: Router,private responsibleservice:ResponsibleService){}
+
+  saveResponsible() {
+    console.log('holi');
+    
+    this.responsibleservice.saveResponsible(this.responsible).subscribe(
+      response  => {
+
+        console.log(this.responsible);
+        alert('Registro exitoso')
+
+        setTimeout(() => {
+
+          this.router.navigate(["/"])
+          
+        }, 1000);
+      }
+    );
+  }
 
 }
--- src/app/services/responsible/responsible.service.ts
+++ src/app/services/responsible/responsible.service.ts
@@ -1,9 +1,43 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Responsible } from 'src/app/models/Responsible';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class ResponsibleService {
 
-  constructor() { }
+
+  constructor(private httpcient:HttpClient) { }
+
+  saveResponsible(newResponsible: Responsible): Observable<Responsible>{
+
+    return this.httpcient.post<Responsible>("http://localhost:8080/responsible/save", newResponsible);
+    
+  }
+
+  listResponsible():  Observable<Responsible>{
+    return this.httpcient.get<Responsible>("http://localhost:8080/responsible/consultall")
+  }
+  
+
+  getResponsible(document : Number):  Observable<Responsible>{
+
+    return  this.httpcient.get<Responsible>(`http://localhost:8080/responsible/consult/${document}`)
+  }
+
+  
+  updateResponsible(doucment:number,responsible: Responsible): Observable<Responsible>{
+
+    console.log(responsible);  
+    return this.httpcient.put<Responsible>(`http://localhost:8080/responsible/update/${responsible.document}`,responsible);  
+  }
+
+  deleteResponsible(document : Number):  Observable<any>{
+
+    return  this.httpcient.delete<any>(`http://localhost:8080/responsible/delete/${document}`)
+  }
+
 }
